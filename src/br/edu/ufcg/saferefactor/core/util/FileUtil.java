bc6868612a1d64b1132a7c3805f68676ce31a92f
Jefferson <jefferson.almeida.comp@gmail.com>
2013-07-20 17:27:56 -0300

    FileUtil mistake correction.

--- src/br/edu/ufcg/saferefactor/core/util/FileUtil.java
+++ src/br/edu/ufcg/saferefactor/core/util/FileUtil.java
@@ -1,7 +1,12 @@
 package br.edu.ufcg.saferefactor.core.util;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -176,4 +181,50 @@ public class FileUtil {
 		File log = new File(dir);
 		log.mkdir();
 	}
+	
+	public static void copyFromTo(File srcFolder, File destFolder){
+		//make sure source exists
+    	if(!srcFolder.exists()){
+           System.out.println("Directory does not exist.");
+        }else{
+           try{
+        	copyFolder(srcFolder,destFolder);
+           }catch(IOException e){
+        	e.printStackTrace();
+           }
+        }
+	}
+	
+	 private static void copyFolder(File src, File dest) throws IOException{
+	    	if(src.isDirectory()){
+	    		//if directory not exists, create it
+	    		if(!dest.exists()){
+	    		   dest.mkdirs();
+	    		   System.out.println("Directory copied from " + src + "  to " + dest);
+	    		}
+	    		//list all the directory contents
+	    		String files[] = src.list();
+	    		for (String file : files) {
+	    		   //construct the src and dest file structure
+	    		   File srcFile = new File(src, file);
+	    		   File destFile = new File(dest, file);
+	    		   //recursive copy
+	    		   copyFolder(srcFile,destFile);
+	    		}
+	    	}else{
+	    		//if file, then copy it
+	    		//Use bytes stream to support all file types
+	    		InputStream in = new FileInputStream(src);
+	    	    OutputStream out = new FileOutputStream(dest); 
+	    	    byte[] buffer = new byte[1024];
+	    	    int length;
+	    	    //copy the file content in bytes 
+	    	    while ((length = in.read(buffer)) > 0){
+	    	    	   out.write(buffer, 0, length);
+	    	    }
+	    	    in.close();
+		        out.close();
+		        System.out.println("File copied from " + src + " to " + dest);
+	    	}
+	    }
 }
