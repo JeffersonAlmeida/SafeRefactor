ad5c82b9c4494056d364eaebb6f81f98e3f6d143
Jefferson <jefferson.almeida.comp@gmail.com>
2013-08-28 14:37:23 -0300

    more reports.

--- src/br/edu/ufcg/saferefactor/core/Saferefactor.java
+++ src/br/edu/ufcg/saferefactor/core/Saferefactor.java
@@ -169,7 +169,7 @@ public class Saferefactor {
 			
 			report = report(printReport);
 			
-			p.executeTarget("coverageReport");
+			//p.executeTarget("coverageReport");
 		}
 		return report.isSameBehavior();		
 	}
@@ -186,7 +186,7 @@ public class Saferefactor {
 			evosuiteProperties.load(is);
 			evosuiteProperties.setProperty("test_dir", "src/evosuite-tests"); // test_dir=src/evosuite-tests
 			evosuiteProperties.setProperty("report_dir", "src/evosuite-report"); // report_dir=src/evosuite-report
-			evosuiteProperties.setProperty("search_budget", "20000"); //30000
+			evosuiteProperties.setProperty("search_budget", "90000"); //30000
 			is.close();
 			OutputStream os = new FileOutputStream(evosuitePropertiesFile);
 			evosuiteProperties.store(os, "changing variable");
